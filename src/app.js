99afe04133dc77e82e7e76e82a87bea8ed79989a
emanuelhardwell <emanuelnoe16@gmail.com>
2020-12-10 12:46:51 -0600

    Server created

---
+++ src/app.js
@@ -0,0 +1,54 @@
+/*  */
+/*  */
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
+const exphbs = require("express-handlebars");
+const morgan = require("morgan");
+const methodOverride = require("method-override");
+const session = require("express-session");
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// required
+
+/* load env */
+dotenv.config({ path: "./configs/config.env" });
+
+// config the app
+const app = express();
+
+//config the port
+app.set("port", process.env.PORT || 3000);
+
+// config the engine
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+  ".hbs",
+  exphbs({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    helpers: path.join(__dirname, "helpers/helper"),
+    extname: ".hbs",
+  })
+);
+app.set("view engine", ".hbs");
+
+// MIDDLEWARE development
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// listen the server
+app.listen(
+  app.get("port"),
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${app.get("port")}`
+  )
+);
