f2d3ce60364a0016b523716232a74d7380d1433f
emanuelhardwell <emanuelnoe16@gmail.com>
2020-12-13 12:34:15 -0600

    Helper created for truncate the large text y for show a tags

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -8,4 +8,19 @@ timeModify.timeago = (timestamp) => {
   return timeago.format(timestamp);
 };
 
+timeModify.truncate = (str, len) => {
+  if (str.length > len && str.length > 0) {
+    let new_str = str + " ";
+    new_str = str.substr(0, len);
+    new_str = str.substr(0, new_str.lastIndexOf(" "));
+    new_str = new_str.length > 0 ? new_str : str.substr(0, len);
+    return new_str + "...";
+  }
+  return str;
+};
+
+timeModify.stripTags = (input) => {
+  return input.replace(/<(?:.|\n)*?>/gm, "");
+};
+
 module.exports = timeModify;
