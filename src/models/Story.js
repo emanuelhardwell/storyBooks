0a10623da3c331b7f39c06c4e396ba0ff6992e6a
emanuelhardwell <emanuelnoe16@gmail.com>
2020-12-12 20:27:00 -0600

    Model of Story created

---
+++ src/models/Story.js
@@ -0,0 +1,14 @@
+/*  */
+/*  */
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const StorySchema = new Schema({
+  title: { type: String, required: true, trim: true },
+  body: { type: String, required: true },
+  status: { type: String, default: "public", enum: ["public", "private"] },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Story", StorySchema);
