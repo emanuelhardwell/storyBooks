74f92f809e151edb72b885373ac55342ea8151e2
emanuelhardwell <emanuelnoe16@gmail.com>
2020-12-13 12:25:31 -0600

    Module edit created

--- src/controllers/story.controller.js
+++ src/controllers/story.controller.js
@@ -5,7 +5,7 @@ const Story = require("../models/Story");
 
 const controller = {};
 
-controller.showFormStory = (req, res) => {
+controller.showFormAddStory = (req, res) => {
   res.render("stories/add");
 };
 
@@ -24,6 +24,33 @@ controller.deleteStory = async (req, res) => {
   try {
     await Story.findByIdAndDelete(req.params.id);
     res.redirect("/dashboard");
+  } catch (error) {
+    console.log(error);
+    res.render("errors/500");
+  }
+};
+
+controller.showFormEditStory = async (req, res) => {
+  try {
+    const searchStory = await Story.findById(req.params.id).lean();
+    res.render("stories/edit", { searchStory });
+  } catch (error) {
+    console.log(error);
+    res.render("errors/500");
+  }
+};
+
+controller.editStory = (req, res) => {
+  res.send("Ok");
+};
+
+controller.showAllStory = async (req, res) => {
+  try {
+    const allStory = await Story.find({ status: "public" })
+      .populate("user")
+      .sort({ createdAt: "desc" })
+      .lean();
+    res.render("stories/all", { allStory });
   } catch (error) {
     console.log(error);
     res.render("errors/500");
--- src/routes/story.routes.js
+++ src/routes/story.routes.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const stories = require("../controllers/story.controller");
+const { isAuthenticated } = require("../helpers/auth");
 
-router.get("/add", stories.showFormStory);
+router.get("/add", isAuthenticated, stories.showFormAddStory);
 
-router.post("/add", stories.addStory);
+router.post("/add", isAuthenticated, stories.addStory);
 
-router.get("/delete/:id", stories.deleteStory);
+router.get("/delete/:id", isAuthenticated, stories.deleteStory);
+
+router.get("/edit/:id", isAuthenticated, stories.showFormEditStory);
+
+router.post("/edit/:id", isAuthenticated, stories.editStory);
+
+router.get("/all", isAuthenticated, stories.showAllStory);
 
 module.exports = router;
